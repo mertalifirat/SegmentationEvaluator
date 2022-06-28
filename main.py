101e7e8524eac2da4a6c8aaacd498d7188aa6bf2
mertalifirat <53237645+mertalifirat@users.noreply.github.com>
2022-06-28 12:02:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from torch.utils.data.dataloader import DataLoader
 ###############
 #### TODO #####
 model=torch.load('') ### .pt file of your model state_dict
-CLASSES=[] ### specify the classes
-ImagesPath='data/data/IJRR17/images/dir_002' ### give the path of your images directory that will be used on testing
-AnnotationsPath='data/data/IJRR17/annotations/dir_002' ### give path of your annotations directory that will be used on testing
-imgSuffix='' ### image file suffix (e.g. .png)
-annSuffix='' ### annotation file suffix (e.g .png)
+CLASSES=[] ### specify the classes  ==>>  [str]
+ImagesPath='' ### give the path of your images directory that will be used on testing ==>> str
+AnnotationsPath='' ### give path of your annotations directory that will be used on testing ==>> str
+imgSuffix='' ### image file suffix (e.g. .png) ==>> str
+annSuffix='' ### annotation file suffix (e.g .png) ==>> str
 ###############
 ###############
 dataset=CustomImageDataset(ImagesPath,AnnotationsPath,imgSuffix,annSuffix,CLASSES)
