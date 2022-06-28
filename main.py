ba3b4f2a81e853c6808eb7e9669873487a133dbf
mertalifirat <53237645+mertalifirat@users.noreply.github.com>
2022-06-28 12:01:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import helperF
 from DatasetBuild import CustomImageDataset
 import torch 
 from torch.utils.data.dataloader import DataLoader
-model=torch.load('tmp.pt')
-CLASSES=['soil','crop','weed']
-dataset=CustomImageDataset('data/data/IJRR17/images/dir_002','data/data/IJRR17/annotations/dir_002',True,'png','png',CLASSES)
+###############
+#### TODO #####
+model=torch.load('') ### .pt file of your model state_dict
+CLASSES=[] ### specify the classes
+ImagesPath='data/data/IJRR17/images/dir_002' ### give the path of your images directory that will be used on testing
+AnnotationsPath='data/data/IJRR17/annotations/dir_002' ### give path of your annotations directory that will be used on testing
+imgSuffix='' ### image file suffix (e.g. .png)
+annSuffix='' ### annotation file suffix (e.g .png)
+###############
+###############
+dataset=CustomImageDataset(ImagesPath,AnnotationsPath,imgSuffix,annSuffix,CLASSES)
 dataload=DataLoader(dataset)
 precision,recall,IoU=helperFuncs.evaluate(model,dataset,dataload)
